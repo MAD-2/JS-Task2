c27e850889e18e7e94fdc3161ef42623ba4ea441
MAD-2 <mmuradiabdullah88@gmail.com>
2021-11-20 17:51:54 +0300

    countdown

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 //Variables
 let time=0;
 let timeId=0;
-let timerOut=true;
+let timerM=true;
 
 //Elements in a variable
-const start=
+const start=document.querySelector("#start");
+const reset=document.querySelector("#reset");
+
+const timerM=document.querySelector('m1');
+let timeSecond=5;
+displayTime(timeSecond)
+const countDown=setInterval(()=>{
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond<=0 || timeSecond<1){
+        endTime();
+        clearInterval(countDown);}},1000)
+    
+function displayTime(second){
+    const min = Math.floor(second/60);
+    const sec = Math.floor(second %60);
+
+    timerM.innerHTML='${min<10 ? "0": ""}{min}:${sec<10?"0":""}${sec}'}
+function endTime(){
+    timerM.innerHTML='TIME OUT'
+}
+
+
 
 //use this function to start the timer
 
