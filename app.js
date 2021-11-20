6eb5f9c1fa41722bcb524a8858ae04300c4f0cda
MAD-2 <mmuradiabdullah88@gmail.com>
2021-11-20 14:38:58 +0300

    task 2

---
+++ app.js
@@ -0,0 +1,16 @@
+//Variables
+let time=0;
+let timeId=0;
+let timerOut=true;
+
+//Elements in a variable
+const start=
+
+//use this function to start the timer
+
+//this function update the timer values
+
+// stop and clear the timer
+
+// Start the timer
+
