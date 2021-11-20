07e7a8738dcbb5a8ed201e237e4b675413320bb6
MAD-2 <mmuradiabdullah88@gmail.com>
2021-11-20 17:59:10 +0300

    --

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Variables
 let time=0;
 let timeId=0;
-let timerM=true;
+let timer=true;
 
 //Elements in a variable
 const start=document.querySelector("#start");
 const reset=document.querySelector("#reset");
 
-const timerM=document.querySelector('m1');
-let timeSecond=5;
+const timerH=document.querySelector('h1');
+let timeSecond=70;
 displayTime(timeSecond)
 const countDown=setInterval(()=>{
     timeSecond--;
@@ -21,9 +21,9 @@ function displayTime(second){
     const min = Math.floor(second/60);
     const sec = Math.floor(second %60);
 
-    timerM.innerHTML='${min<10 ? "0": ""}{min}:${sec<10?"0":""}${sec}'}
+    timerH.innerHTML='${min<10 ? "0": ""}{min}:${sec<10?"0":""}${sec}'}
 function endTime(){
-    timerM.innerHTML='TIME OUT'
+    timerH.innerHTML='TIME OUT'
 }
 start.addEventListener("click",function(){
     initClock();});
